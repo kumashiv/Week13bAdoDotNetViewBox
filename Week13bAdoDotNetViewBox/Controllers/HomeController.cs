276d208027935a2052865ecb3078a8b08b2c0405
shiv kumar <kumarshiv_sk@outlook.com>
2025-03-23 22:35:48 -0400

    Object created in controller to connect with model

--- Week13bAdoDotNetViewBox/Controllers/HomeController.cs
+++ Week13bAdoDotNetViewBox/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Week13bAdoDotNetViewBox.Models;
 
 namespace Week13bAdoDotNetViewBox.Controllers
 {
+
+
     public class HomeController : Controller
     {
+
+        Week13bAdoDotNetViewBoxDBEntities _db = new Week13bAdoDotNetViewBoxDBEntities();    // Object from Model1.Context.cs
+
         public ActionResult Index()
         {
             return View();
